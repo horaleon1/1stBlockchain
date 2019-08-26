3601d794ccc74d27e4244ac8d0826e5941a5bedd
horaleon1 <horaleon1@gmail.com>
2019-08-26 16:07:42 -0500

    first

---
+++ block.test.js
@@ -0,0 +1,7 @@
+describe('Block', () => {
+  const timestamp = 'a-date';
+  const lastHash = 'foo-hash';
+  const hash = 'bar-hash';
+  const data = ['blockchain', 'data']; 
+  const block = new Block ({ timestamp, lastHash, hash, data})
+})
